c2937c4a024ae37b9edf31b71321536d350fa154
Roberto Tellez <A01374866@itesm.mx>
2021-05-15 22:55:03 -0500

    Add and rearrange imports in app module

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,30 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
 
 import { AppComponent } from './app.component';
+import { APP_ROUTING } from './app.routes';
+
 import { EncabezadoComponent } from './componentes/comunes/encabezado/encabezado.component';
 import { PrincipalComponent } from './componentes/principal/principal.component';
 import { AcercadeComponent } from './componentes/acercade/acercade.component';
 import { ConsolasComponent } from './componentes/consolas/consolas.component';
-import { APP_ROUTING } from './app.routes';
 import { InfoconsolaComponent } from './componentes/infoconsola/infoconsola.component';
 import { JuegosconsolaComponent } from './componentes/juegosconsola/juegosconsola.component';
 import { JuegoComponent } from './componentes/juego/juego.component';
-
-import { ConsolasService } from './servicios/consolas.service';
 import { ResconsolasComponent } from './componentes/buscador/resconsolas/resconsolas.component';
 import { ResjuegosComponent } from './componentes/buscador/resjuegos/resjuegos.component';
 import { BlogComponent } from './componentes/blog/blog.component';
 import { AdministracionComponent } from './componentes/administracion/administracion.component';
 import { FormConsolaComponent } from './componentes/administracion/form-consola/form-consola.component';
 import { FormJuegoComponent } from './componentes/administracion/form-juego/form-juego.component';
-import { HttpClientModule } from '@angular/common/http';
-import { GamesService } from './servicios/games.service';
-import { BlogService } from './servicios/blog.service';
 import { ChatComponent } from './componentes/chat/chat.component';
 import { MensajeComponent } from './componentes/chat/mensaje/mensaje.component';
 import { MensajeschatComponent } from './componentes/chat/mensajeschat/mensajeschat.component';
+
+import { ConsolasService } from './servicios/consolas.service';
+import { GamesService } from './servicios/games.service';
+import { BlogService } from './servicios/blog.service';
+import { ChatService } from './servicios/chat.service';
+
+
+const config: SocketIoConfig = { url: 'http://localhost:8585', options: {} };
 
 @NgModule({
   declarations: [
@@ -51,12 +58,14 @@ import { MensajeschatComponent } from './componentes/chat/mensajeschat/mensajesc
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
+    SocketIoModule.forRoot(config),
     APP_ROUTING
   ],
   providers: [
     ConsolasService,
     GamesService,
-    BlogService
+    BlogService,
+    ChatService
   ],
   bootstrap: [AppComponent]
 })
